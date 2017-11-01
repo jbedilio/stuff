b0bf0c9d08e4b665fc7e1387c9c0cf735941456c
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-10-31 23:26:06 -0400

    building the server & controller & view

--- controllers/ridezController.js
+++ controllers/ridezController.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var request = require('request');
+const express = require('express');
+const request = require('request');
 //grab an instance of express router
-var router = express.Router();
-var mongoose = require('mongoose');
+const router = express.Router();
+const mongoose = require('mongoose');
 
 //import the models
 var User = require('./../models/UserModel.js');
 var Rating = require('./../models/RatingModel.js');
 
 router.get('/', (req, res) => {
-    res.render('index', hbsObject = {data: "Hello Fuckers!"});
+    res.render('root');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var bp = require('body-parser');
-var mongoose = require('mongoose');
-var request = require('request');
+const express = require('express');
+const bp = require('body-parser');
+const mongoose = require('mongoose');
+const request = require('request');
 
 //setting mongoose to leverage built in ES6 Promises
 mongoose.Promise = Promise;
@@ -10,10 +10,10 @@ mongoose.Promise = Promise;
 var PORT = process.env.PORT || 3000;
 
 //grabbing an instance of express
-var app = express();
+const app = express();
 
 //serve static content for the app from the "public" directory in the app directory
-app.use(express.static('public'));
+app.use(express.static('client/build'));
 
 //setting the instance of express to use body-parser
 app.use(bp.urlencoded({extended: true}));
@@ -25,7 +25,7 @@ if(process.env.MONGODB_URI) {
     mongoose.connect('mongodb://localhost/ridezdb', {useMongoClient: true});
 };
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 
 db.on('error', error => {
     console.log('Mongoose error: ', error);
@@ -35,14 +35,10 @@ db.once('open', () => {
     console.log('Mongoose in the hizzy!');
 });
 
-//set handlebars
-var exphbs = require('express-handlebars');
-
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
-var routes = require('./controllers/ridezController.js');
+const routes = require('./controllers/ridezController.js');
 
 app.use('/', routes);
 
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`axios ==> API Server now listening on PORT ${PORT}!`);
+});
